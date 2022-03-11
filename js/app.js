b8542870be50242b7feb732e9a48e8674dd7edfb
Rizvimahbub <rizvialam007@gmail.com>
2022-03-11 18:31:41 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -16,7 +16,8 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-    likedPostsId.plus(id); 
+    likedPostsId.push(id);
+    document.getElementById('heart').style.color = 'red'; 
     showPosts(posts);
 };
 
@@ -86,7 +87,7 @@ const createPost = (post) => {
               <div class="post__footer">
                 <div class="post__buttons">
                   <button class="post__button" onclick="addToLiked(${post.id})">
-                  <i class="fa-solid fa-heart ${isLiked(post.id) && "text-danger"}"></i>
+                  <i id="heart" class="fa-solid fa-heart ${isLiked(post.id) && "text-danger"}"></i>
                     
                   </button>
                   <button class="post__button">
